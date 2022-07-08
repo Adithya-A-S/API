f875ee7fe66646970f131517da13658890e3dc74
Adithya-A-S <hariadithya78@gmail.com>
2022-07-09 02:53:56 +0530

    master

--- flaskapi.py
+++ flaskapi.py
@@ -1,11 +1,11 @@
 import numpy as np
 from flask import Flask, request, jsonify
-
+import pickle
 from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
 
-
+model = pickle.load(open('./model.pkl', 'rb'))
 
 
 @app.route('/welcome', methods=['GET'])
@@ -22,7 +22,19 @@ def homepage():
     <img src="https://media.giphy.com/media/J1RWP1OyfkwATrL9cd/giphy.gif"/>
     """
 
+@app.route('/prediction', methods=['POST'])
+@cross_origin()
+def predict():
+    payload = request.get_json(force=True)
+    prediction = model.predict([np.array(list(payload.values())+[0.3])]).tolist()[0]
 
+    output = {
+        "data": {
+            'prediction': prediction,
+            'interpretation': 'have high chances of getting diabetes' if prediction == 1 else 'you are healthy!'
+        }
+    }
+    return jsonify(output)
 
 
 
