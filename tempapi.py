975fb09b6ed2ecb79501a957c3827054600f47ef
Adithya-A-S <hariadithya78@gmail.com>
2022-11-16 22:00:59 +0530

    commit

--- tempapi.py
+++ tempapi.py
@@ -1,15 +1,29 @@
-import numpy as np
-from flask import Flask, request, jsonify
 import pickle
+
+import numpy as np
+from flask import Flask, jsonify, request
 from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
 
+model = pickle.load(open('./model.pkl', 'rb'))
 
 
 
-@app.route('/', methods=['GET'])
+@app.route('/prediction', methods=['POST'])
 @cross_origin()
-def home():
-    return {"data": "Welcome to online diabetes prediction model"}
+def predict():
+    payload = request.get_json(force=True)
+    prediction = model.predict([np.array(list(payload.values())+[0.3])]).tolist()[0]
+
+    output = {
+        "data": {
+            'prediction': prediction,
+            'interpretation': 'have high chances of getting diabetes' if prediction == 1 else 'you are healthy!'
+        }
+    }
+    return jsonify(output)
+
+
+
 
